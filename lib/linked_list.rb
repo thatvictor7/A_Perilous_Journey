9b36394ceffd4c5de2cb06ca0ae1dffa3579f97a
thatvictor7 <montoya.victor7@gmail.com>
2018-01-29 18:11:00 -0700

    added to_string method

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,16 +1,24 @@
 class LinkedList
-  attr_reader :head
+  attr_reader :head, :count
   def initialize
     @head = nil
+    @count = 0
+    @name = []
   end
 
   def append(data)
+    @name.push(data)
     node = node_test(data)
     @head = node
+    @count += 1
     node
   end
 
   def node_test(surname)
     Node.new(surname)
+  end
+
+  def to_string
+    "The #{@name.first} family"
   end
 end
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -21,6 +21,22 @@ class LinkedListTest < Minitest::Test
     list
   end
 
-  
+  def test_head_next_node_is_nil
+    list = LinkedList.new
+    list.append("West")
+    assert_nil list.head.next_node
+  end
+
+  def test_list_count_at_one
+    list = LinkedList.new
+    list.append("West")
+    assert_equal 1, list.count
+  end
+
+  def test_to_string_of_append_data
+    list = LinkedList.new
+    list.append("West")
+    assert_equal "The West family", list.to_string
+  end
 
 end
